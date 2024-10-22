f8fb1d319c064a82201e6a478ef9e96e50e09509
Ding <zhankit1209@gmail.com>
2024-10-22 10:50:18 +0800

    demo: test

---
+++ uxpin.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  components: {
+    categories: [
+      {
+        name: 'Button',
+        include: ['src/components/Button/Button.tsx'],
+      },
+      {
+        name: 'Card',
+        include: ['src/components/Card/Card.tsx'],
+      },
+    ],
+  },
+  name: 'React Native Paper',
+};
